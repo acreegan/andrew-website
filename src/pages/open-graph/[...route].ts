1a6d680701d7ce200761f357a3a9e94e8daf9bd3
acreegan <andrew.s.creegan@gmail.com>
2026-02-26 15:40:23 +1100

    fix build errors

--- src/pages/open-graph/[...route].ts
+++ src/pages/open-graph/[...route].ts
@@ -1,46 +1,49 @@
 import { getCollection } from 'astro:content';
 import { OGImageRoute } from 'astro-og-canvas';
 
-const collectionEntries = await getCollection('projects');
+const posts = await getCollection('projects');
 
-// Map the array of content collection entries to create an object.
-// Converts [{ id: 'post.md', data: { title: 'Example', description: '' } }]
-// to { 'post.md': { title: 'Example', description: '' } }
-const pages = Object.fromEntries(collectionEntries.map(({ id, data }) => [id, data]));
+const pages = Object.fromEntries(
+  posts.map((post) => {
+    const imagePath = post.data.thumbnail ? post.data.thumbnail.src.src.split("?")[0].split("@fs/")[1] : undefined
+    return [
+      post.id,
+      {
+        title: post.data.title,
+        description: post.data.description,
+        thumbnailPath: imagePath,
+      }
+    ];
+  })
+);
 
 export const { getStaticPaths, GET } = await OGImageRoute({
-  // Tell us the name of your dynamic route segment.
-  // In this case it’s `route`, because the file is named `[...route].ts`.
   param: 'route',
 
   pages: pages,
 
-  getImageOptions: (path, page) => ({
+  getImageOptions: (_path, page) => ({
     title: page.title,
     description: page.description,
     logo: {
       path: './public/favicon_light.svg',
     },
-    bgImage: {
-      path: `${page.thumbnail?.src.src.split("?")[0].split("@fs/")[1]}`,
+    bgImage: page.thumbnailPath? {
+      path: page.thumbnailPath,
       fit: 'cover'
-    },
-// RGB only: [Red, Green, Blue]
-    // To make it look "transparent," use very dark grays/blacks
+    }:undefined ,
     bgGradient: [[10, 10, 10], [30, 30, 30]], 
-    
     font: {
       title: { 
-        color: [255, 255, 255], // White
+        color: [255, 255, 255],
         size: 60, 
         weight: 'Bold' 
       },
       description: { 
-        color: [200, 200, 200], // Light Gray
+        color: [200, 200, 200],
         size: 30 
       },
     },
-    // This helps the text breathe if the background image is busy
     padding: 80,
   }),
 });
